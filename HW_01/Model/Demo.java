e1abbc1c8dcaa0394c6c6aded4240f3875e5eb57
DKiyash <dkiyashenko@gmail.com>
2022-09-29 10:52:53 +0200

    Task 1.1 solved

---
+++ HW_01/Model/Demo.java
@@ -0,0 +1,11 @@
+package java_35e_HW.HW_01.Model;
+
+public class Demo {
+    public static void main(String[] args) {
+        Rectangle rectangle1 = new Rectangle(4, 5);
+        Rectangle rectangle2 = new Rectangle(5, 8);
+
+        rectangle1.outputArea();
+        rectangle2.outputArea();
+    }
+}
--- HW_01/Model/Rectangle.java
+++ HW_01/Model/Rectangle.java
@@ -1,4 +1,22 @@
 package java_35e_HW.HW_01.Model;
 
 public class Rectangle {
+    private int width;
+    private int leight;
+
+    public Rectangle(int width, int leight) {
+        this.width = width;
+        this.leight = leight;
+    }
+
+    private int Area(int width, int leight) {
+        return width * leight * 2;
+    }
+
+    public void outputArea() {
+        System.out.println("Длина: " + leight + ", ширина: " + width);
+        System.out.println("Площадь: " + Area(width, leight));
+        System.out.println("Периметр: " + 2 * (width + leight));
+        System.out.println();
+    }
 }
