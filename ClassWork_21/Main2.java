4d5cfaed5bf425a3996fb008ed9ae0e84a78f55a
DKiyash <dkiyashenko@gmail.com>
2022-12-12 20:08:45 +0100

    Classwork_21 added

---
+++ ClassWork_21/Main2.java
@@ -0,0 +1,36 @@
+package java_35e_HW.ClassWork_21;
+
+import java.io.*;
+
+public class Main2 {
+    public static void main(String[] args) throws IOException {
+        File fileA = new File("./A.txt");
+        File fileB = new File("./B.txt");
+        FileWriter writerFileA = new FileWriter(fileA);
+        writerFileA.write(", my name is Dima");
+        writerFileA.flush();
+        writerFileA.close();
+        FileWriter writerFileB = new FileWriter(fileB);
+        writerFileB.write("Hello world");
+        writerFileB.flush();
+        writerFileB.close();
+
+        FileOutputStream fos = new FileOutputStream(fileB, true);
+        FileInputStream fis = new FileInputStream(fileA);
+        fis.transferTo(fos);
+        fos.flush();
+        fis.close();
+        fos.close();
+//        out.write();
+    }
+}
+
+//TODO
+// Используя FileInputStream, прочитайте данные из файла A
+// И с помощью FileOutputStream допишете их в конец файла B
+
+// File A: ", my name is <ваше имя>"
+// File B: "Hello world"
+
+//fis.transferTo(fos)
+
