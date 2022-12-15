9aca031631adb339a3a4a6d1ce50d6d34d1ce511
DKiyash <dkiyashenko@gmail.com>
2022-12-15 19:28:42 +0100

    Classwork 23 added

--- ClassWork_23/Main.java
+++ ClassWork_23/Main.java
@@ -2,16 +2,17 @@ package java_35e_HW.ClassWork_23;
 
 public class Main {
     public static void main(String[] args) {
-        int i = 123456789;
-        System.out.println(separ(i));
+       System.out.println("2000 -> " + task4(2000));
+       System.out.println("6000000 -> " + task4(6000000));
     }
 
-    public static String separ(int n) {
+    public static String task4 (int n) {
         String num = Integer.toString(n);
         int len = num.length();
         if(len < 4) {
             return num;
         }
-        return separ(Integer.parseInt(num.substring(0, len-3))) + ',' + num.substring(len-3);
+        return task4(Integer.parseInt(num.substring(0, len-3))) + ',' + num.substring(len-3);
     }
 }
+// 2000 -> 2,000 6000000 -> 6,000,000
